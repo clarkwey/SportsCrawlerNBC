6917d969168d73d7dbd717a91f61b3945d4ccaa3
clarkwey@gmail.com <clarkwey@gmail.com>
2018-06-02 13:21:57 -0700

    updates to the graph

---
+++ DrawingPanel/SwingWorkerRealTime.java
@@ -0,0 +1,182 @@
+import java.util.*;
+import java.net.*;
+import java.io.*;
+import javax.json.*;
+import javax.swing.SwingWorker;
+import org.knowm.xchart.QuickChart;
+import org.knowm.xchart.SwingWrapper;
+import org.knowm.xchart.XYChart;
+//javac -cp ".:./*" SwingWorkerRealTime.java
+//java -cp ".:./*" SwingWorkerRealTime
+/** Creates a real-time chart using SwingWorker */
+public class SwingWorkerRealTime {
+private static HttpURLConnection httpConn;
+private static double lastPrice;
+//private static final DrawingPanel panel = new DrawingPanel(3000,3000);
+//private static final Graphics g = panel.getGraphics();
+private static double[] marketData = new double[1000]; //Creates the temp array for each refresh of data
+private static ArrayList<ArrayList<Double>> plotPoints = new ArrayList<>(); //Records each average market value and stores it
+  MySwingWorker mySwingWorker;
+  SwingWrapper<XYChart> sw;
+  XYChart chart;
+  //ArrayList<ArrayList<Double>> plotPoints = new ArrayList<>();
+/*
+  public static void main(String[] args) throws Exception {
+
+    SwingWorkerRealTime swingWorkerRealTime = new SwingWorkerRealTime();
+    swingWorkerRealTime.go();
+  }
+*/
+
+  public void go() {
+
+    // Create Chart
+    chart =
+        QuickChart.getChart(
+            "Market Data Live",
+            "Time",
+            "Value_USD",
+            "randomWalk",
+            new double[] {0},
+            new double[] {0});
+    chart.getStyler().setLegendVisible(false);
+    chart.getStyler().setXAxisTicksVisible(false);
+
+    // Show it
+    sw = new SwingWrapper<XYChart>(chart);
+    sw.displayChart();
+
+    mySwingWorker = new MySwingWorker();
+    mySwingWorker.execute();
+  }
+
+  private class MySwingWorker extends SwingWorker<Boolean, double[]> {
+
+    final ArrayList<ArrayList<Double>> plotPoints = new ArrayList<>();
+
+    public MySwingWorker() {
+      ArrayList<Double> topBollinger = new ArrayList<Double>();
+      ArrayList<Double> SMA = new ArrayList<Double>();
+      ArrayList<Double> bottomBollinger = new ArrayList<Double>();
+      plotPoints.add(topBollinger);//Top Bollinger Band
+      plotPoints.add(SMA);//SMA
+      plotPoints.add(bottomBollinger);//Bottom Bollinger Band
+    }
+
+    @Override
+    protected Boolean doInBackground() throws Exception {
+
+      while (!isCancelled()) {
+        //System.out.println("hi");
+        sendGetRequest(generateUrl());
+
+        double[] array0 = new double[plotPoints.get(0).size()];
+        double[] array1 = new double[plotPoints.get(1).size()];
+        double[] array2 = new double[plotPoints.get(2).size()];
+        for (int i = 0; i < plotPoints.get(0).size(); i++) {
+          array0[i] = plotPoints.get(0).get(i);
+        }
+        for (int j = 0; j < plotPoints.get(1).size(); j++) {
+          array1[j] = plotPoints.get(1).get(j);
+        }
+        for (int k = 0; k < plotPoints.get(2).size(); k++) {
+          array2[k] = plotPoints.get(2).get(k);
+        }
+        publish(array0);
+        publish(array1);
+        publish(array2);
+        try {
+          Thread.sleep(5);
+        } catch (InterruptedException e) {
+          // eat it. caught when interrupt is called
+          System.out.println("MySwingWorker shut down.");
+        }
+      }
+
+      return true;
+    }
+
+    @Override
+    protected void process(List<double[]> chunks) {
+
+      //System.out.println("number of chunks: " + chunks.size());
+
+      double[] mostRecentDataSet = chunks.get(chunks.size() - 1);
+
+      chart.updateXYSeries("randomWalk", null, mostRecentDataSet, null);
+      sw.repaintChart();
+
+      long start = System.currentTimeMillis();
+      long duration = System.currentTimeMillis() - start;
+      try {
+        Thread.sleep(40 - duration); // 40 ms ==> 25fps
+        // Thread.sleep(400 - duration); // 40 ms ==> 2.5fps
+      } catch (InterruptedException e) {
+        System.out.println("InterruptedException occurred.");
+      }
+    }
+    protected String generateUrl(){
+      //Let's create an 20 - period SMA where each period is 10 minutes long
+
+      long currentUnixTime = System.currentTimeMillis() / 1000L;
+      // 12000 = 10 minutes period * 60s/period * 20 periods
+      long startUnixTime = currentUnixTime - 12000;
+      String url = "https://poloniex.com/public?command=returnTradeHistory&currencyPair=USDT_BTC&start=" + startUnixTime + "&end=" + currentUnixTime;
+      //System.out.println(url);
+      return url;
+    }
+    protected HttpURLConnection sendGetRequest(String requestURL){
+      try{
+
+        URL url = new URL(requestURL);
+        httpConn = (HttpURLConnection) url.openConnection();
+        httpConn.setUseCaches(false);
+        httpConn.setDoInput(true); // true if we want to read server's response
+        httpConn.setDoOutput(false); // false indicates this is a GET request
+        filter(); //Moves rates from json into array
+
+        double currentAverage = movingAverage();
+        //System.out.print(currentAverage);//Calculates and prints the moving average
+        //System.out.println();
+        //httpConn.disconnect();
+        //tesBuy(currentAverage);
+        return httpConn;
+      }catch (IOException e){
+        System.err.println("IOException: "+e.getMessage());
+      }
+      return httpConn;
+    }
+    protected void filter() throws IOException{
+      JsonReader reader = Json.createReader(new InputStreamReader(httpConn.getInputStream(), "UTF-8"));
+      JsonArray jo = reader.readArray();
+      for (int i = 0; i < 1000; i++){
+        JsonObject jop = jo.getJsonObject(i);
+        marketData[i] = (Double.parseDouble(jop.getString("rate")));
+
+      }
+      lastPrice = marketData[999];
+    }
+
+    protected double movingAverage(){
+      double sum = 0;
+      for (double num : marketData){
+        sum = sum + num;
+      }
+      double average = sum / 1000;
+      double std = 0;
+
+      for (double num : marketData){
+        std = std + Math.pow(num - average , 2);
+      }
+
+      std = Math.sqrt(std / 1000);
+
+      plotPoints.get(0).add(2 * std + average);
+      plotPoints.get(1).add(average);
+      plotPoints.get(2).add(-1 * 2 * std + average);
+      //System.out.println("std: " + std);
+
+      return average;
+    }
+  }
+}
--- DrawingPanel/Writer.java
+++ DrawingPanel/Writer.java
@@ -9,9 +9,10 @@ import java.awt.*;
 //java -cp ".:./*" Writer
 public class Writer{
   public static boolean HOLD = false;
-  private static final DrawingPanel panel = new DrawingPanel(800,3000);
-  private static final Graphics g = panel.getGraphics();
-  private static double[] marketData = new double[150]; //Creates the temp array for each refresh of data
+  private static double lastPrice;
+  //private static final DrawingPanel panel = new DrawingPanel(3000,3000);
+  //private static final Graphics g = panel.getGraphics();
+  private static double[] marketData = new double[1000]; //Creates the temp array for each refresh of data
   private static ArrayList<ArrayList<Double>> plotPoints = new ArrayList<>(); //Records each average market value and stores it
   private static HttpURLConnection httpConn; //Universal HttpURLConnection
   private static final String POLONIEX_SECRET_KEY = "c251d9e12a35f8fe4cc04c2399ca5069d01205f3145d77097c3a38716362be19727420825c6df759e39441cd7c14f0a4f530eb9024a18f495e743dee49d79427"; //KEY
@@ -31,15 +32,25 @@ public class Writer{
     //String url = "https://poloniex.com/public?command=returnTradeHistory&currencyPair=USDT_"+cryptocurrency;
     //How many seconds per refresh... should be around 600
     int seconds = 1;
+    int whenToPrint = 0;
     //The infinite loop
     initializeList();
+    SwingWorkerRealTime swingWorkerRealTime = new SwingWorkerRealTime();
+    swingWorkerRealTime.go();
     while (true){
-      SimpleDateFormat ft = new SimpleDateFormat ("MM:dd:YY hh:mm:ss a zzz");
-      System.out.print("Current Date/Time: " + ft.format(new Date())+" : Period " + seconds+ "s: ");
-      sendGetRequest(generateUrl());
-
-      Thread.sleep(seconds*1000);
+      SimpleDateFormat ft = new SimpleDateFormat ("MM/dd/YY hh:mm:ss a zzz");
+      //sendGetRequest(generateUrl());
+      //We want to make a request every second BUT only print out the results every 10 seconds
+      //plotPoints();
+      if (whenToPrint == 10){
+        //System.out.print("Current Date/Time: " + ft.format(new Date())+" : " + plotPoints.get(1).get(plotPoints.get(1).size()-1) + "\n");
+        System.out.println("Last Price: " + lastPrice);
+        //System.out.println(plotPoints.get(0).get(plotPoints.get(0).size()-1) + " " + plotPoints.get(2).get(plotPoints.get(2).size()-1));
+        whenToPrint = 0;
       }
+      whenToPrint++;
+      Thread.sleep(seconds*1000);
+    }
   }
   private static void initializeList(){
     ArrayList<Double> topBollinger = new ArrayList<Double>();
@@ -55,6 +66,7 @@ public class Writer{
     // 12000 = 10 minutes period * 60s/period * 20 periods
     long startUnixTime = currentUnixTime - 12000;
     String url = "https://poloniex.com/public?command=returnTradeHistory&currencyPair=USDT_BTC&start=" + startUnixTime + "&end=" + currentUnixTime;
+    //System.out.println(url);
     return url;
   }
   private static HttpURLConnection sendGetRequest(String requestURL){
@@ -66,8 +78,8 @@ public class Writer{
       httpConn.setDoOutput(false); // false indicates this is a GET request
       filter(); //Moves rates from json into array
       double currentAverage = movingAverage();
-      System.out.print(currentAverage);//Calculates and prints the moving average
-      System.out.println();
+      //System.out.print(currentAverage);//Calculates and prints the moving average
+      //System.out.println();
       //httpConn.disconnect();
       //tesBuy(currentAverage);
       return httpConn;
@@ -80,10 +92,11 @@ public class Writer{
   private static void filter() throws IOException{
     JsonReader reader = Json.createReader(new InputStreamReader(httpConn.getInputStream(), "UTF-8"));
     JsonArray jo = reader.readArray();
-    for (int i = 0; i < 150; i++){
+    for (int i = 0; i < 1000; i++){
       JsonObject jop = jo.getJsonObject(i);
       marketData[i] = (Double.parseDouble(jop.getString("rate")));
     }
+    lastPrice = marketData[999];
   }
   //Method calculates moving average and standard deviations for bollinger bands
   private static double movingAverage(){
@@ -91,17 +104,30 @@ public class Writer{
     for (double num : marketData){
       sum = sum + num;
     }
-    double average = sum / 150;
+    double average = sum / 1000;
     double std = 0;
     for (double num : marketData){
       std = std + Math.pow(num - average , 2);
     }
+    std = Math.sqrt(std / 1000);
     plotPoints.get(0).add(2 * std + average);
     plotPoints.get(1).add(average);
-    plotPoints.get(2).add(2 * std - average);
+    plotPoints.get(2).add(-1 * 2 * std + average);
+    //System.out.println("std: " + std);
     return average;
   }
   //TODO create an alert to buy or testSell
+  /*
+  The use of Bollinger Bands varies widely among traders. Some traders buy when
+  price touches the lower Bollinger Band and exit when price touches the moving
+  average in the center of the bands. Other traders buy when price breaks above
+  the upper Bollinger Band or sell when price falls below the lower Bollinger
+  Band.[4] Moreover, the use of Bollinger Bands is not confined to stock traders;
+  options traders, most notably implied volatility traders, often sell options
+  when Bollinger Bands are historically far apart or buy options when the Bollinger
+  Bands are historically close together, in both instances, expecting volatility
+  to revert towards the average historical volatility level for the stock.
+  */
   /*
   private static boolean testSell(double currentAverage){
     for (double average : averageList){
@@ -110,14 +136,18 @@ public class Writer{
   }
   */
   //private static final DrawingPanel panel = new DrawingPanel(1000,3000);
+  /*
   public static void plotPoints(){
     for (int i = 0; i < plotPoints.get(1).size(); i++){
       double tempTop = 3000 - (plotPoints.get(0).get(i) - 6000);
       double tempMiddle = 3000 - (plotPoints.get(1).get(i) - 6000);
       double tempBottom = 3000 - (plotPoints.get(2).get(i) - 6000);
-      g.fillOval(i*10, (int) tempTop , 1, 1);
-      g.fillOval(i*10, (int) tempMiddle , 1, 1);
-      g.fillOval(i*10, (int) tempBottom , 1, 1);
+      System.out.println(tempTop + " " + tempMiddle + " "+ " " + tempBottom);
+      g.fillOval(i, (int) tempTop, 1, 1);
+      g.fillOval(i, (int) tempMiddle, 1, 1);
+      g.fillOval(i, (int) tempBottom, 1, 1);
+      g.fillOval(i, (int) lastPrice, 1, 1);
     }
   }
+  */
 }
